4dbb48821a75d47887a8c7a64c2eb81dd76e35ac
Jeremy Novak <jgn@jgn.dev>
2024-09-01 22:57:45 -0500

    working on terraform deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  # Use the latest 3.x version
+    }
+  }
+}
+
 # Provider configuration
 provider "aws" {
   region = var.aws_region
