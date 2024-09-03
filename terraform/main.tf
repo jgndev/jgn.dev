38eb7d28d184f57c7581224a84e074faba140714
Jeremy Novak <jgn@jgn.dev>
2024-09-03 11:00:09 -0500

    create new bucket for public images used in posts

--- terraform/main.tf
+++ terraform/main.tf
@@ -209,7 +209,7 @@ resource "aws_s3_bucket_policy" "public_img_bucket_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.resume_bucket.arn}/*"
+        Resource  = "${aws_s3_bucket.public_img_bucket.arn}/*"
       },
     ]
   })
