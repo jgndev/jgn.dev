b6bcb7d50caa7af476528b10e37804d5e6da0891
Jeremy Novak <jgn@jgn.dev>
2024-09-23 11:40:13 -0500

    generate templ files

--- internal/views/lockups/languages_templ.go
+++ internal/views/lockups/languages_templ.go
@@ -46,7 +46,7 @@ func Languages() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = icons.Cpp().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = icons.Zig().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
